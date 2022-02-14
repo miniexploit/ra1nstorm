39b7f9da50bd4d8f55490e943bb753f3c6371023
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-02-14 12:19:25 +0700

    Verify the iPSW to be valid to restoring the connected device

--- others/ipsw.py
+++ others/ipsw.py
@@ -4,13 +4,14 @@ import sys, os
 import hashlib
 import json
 import shutil
+import os, sys
+from others.error import retassure, reterror
 from zipfile import ZipFile, is_zipfile
 
 
 class IPSW:
 	def __init__(self, ipsw: Path):
 		self.ipsw = ipsw
-		self.verify_ipsw()
 	def __str__(self) -> str:
 		return str(self.ipsw)
 
@@ -19,24 +20,23 @@ class IPSW:
 			with ZipFile(self.ipsw, 'r') as ipsw:
 				return ipsw.read(file)
 		except KeyError:
-			sys.exit(f"[ERROR] File not in IPSW: {file}")
+			reterror(f"File not in IPSW: {file}")
 	def extract_file(self, file, location):
 		try:
 			print(f"Extracting '{file}'...")
 			with ZipFile(self.ipsw, 'r') as ipsw, open(location, 'wb') as f:
 				f.write(ipsw.read(file))
 		except:
-			sys.exit(f"[ERROR] Couldn't extract {file}")
-	def verify_ipsw(self):
-		if not os.path.exists(self.ipsw):
-			sys.exit(f"[ERROR] iPSW not found at: {self.ipsw}")
-		if not is_zipfile(self.ipsw):
-			sys.exit(f"[ERROR] '{self.ipsw}' is not a valid iPSW")
+			reterror(f"Couldn't extract {file}")
+	def verify_ipsw(self, identifier, buildmanifest):
+		print("Verifying iPSW...")
+		retassure(os.path.exists(self.ipsw), f"iPSW not found at: {self.ipsw}")
+		retassure(is_zipfile(self.ipsw), f"'{self.ipsw}' is not a valid iPSW")
+		retassure(identifier in buildmanifest.supported_devices, "This iPSW can't be used to restore the connected device. Exiting.")
 	def get_ipswinfo(self, buildmanifest):
 		ios_ver = ""
 		for ver in buildmanifest.version:
 			ios_ver += f"{ver}."
 		ios_ver = ios_ver[:-1]
 		print(f"iPSW info: Version: {ios_ver} BuildID: {buildmanifest.get_buildid()}")
-		if not buildmanifest.version[0] >= 11 or not buildmanifest.version[0] <= 14:
-			sys.exit(f"[ERROR] iOS {buildmanifest.version[0]}.{buildmanifest.version[1]} is not supported. Exiting.")
+		retassure(11 <= buildmanifest.version[0] <= 14, f"iOS {buildmanifest.version[0]}.{buildmanifest.version[1]} is not supported. Exiting.")
