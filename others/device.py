5a6f1f35d4df1fd598d07e2291cad1e6c3f2c802
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-02-14 12:21:55 +0700

    just a small update, ignore it

--- others/device.py
+++ others/device.py
@@ -2,6 +2,16 @@ import subprocess
 import sys
 import subprocess
 import usb.core
+from others.error import retassure, reterror
+
+
+def issupported(identifier):
+	supported_devices = [
+	'iPhone6,1', 'iPhone6,2', 'iPhone7,1', 'iPhone7,2', 'iPhone8,1', 'iPhone8,2', 'iPhone9,1', 'iPhone9,2', 'iPhone9,3', 'iPhone9,4', 'iPhone10,1', 'iPhone10,2', 'iPhone10,3', 'iPhone10,4', 'iPhone10,5', 'iPhone10,6',
+	# iPad
+	'iPad4,1', 'iPad4,2', 'iPad4,3', 'iPad4,5', 'iPad4,6', 'iPad4,8', 'iPad4,9', 'iPad5,1', 'iPad5,2', 'iPad5,4', 'iPad6,4', 'iPad6,8', 'iPad7,2', 'iPad7,4', 'iPad8,3', 'iPad8,4', 'iPad8,7', 'iPad8,8', 'iPad8,10', 'iPad8,12', 'iPad11,2', 'iPad11,4', 'iPad13,2'
+	]
+	return identifier in supported_devices
 def get_devinfo():
 	out = subprocess.run(('irecovery','-q'), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, universal_newlines=True).stdout
 		
@@ -12,22 +22,16 @@ def get_devinfo():
 			identifier = st.replace("PRODUCT: ", "")
 		if "ECID" in st:
 			ecid = st.replace("ECID: ", "")
-			ecid = st.replace("ECID: ", "")
 		if "NONC" in st:
 			apnonce = st.replace("NONC: ","")
-	try:
-		return identifier, identifier[:identifier.find(",")].lower(), board, ecid, apnonce
-	except UnboundLocalError:
-		sys.exit("[ERROR] Unable to get device info, make sure irecovery version is >= 1.0.1. Exiting.")
+	return identifier, board, ecid, apnonce
 def detect_device():
 	device = usb.core.find(idVendor=0x5AC, idProduct=0x1227)
-	if device is None:
-		sys.exit("[ERROR] A DFU device was not found. Exiting.")
+	retassure(device is not None, "A DFU device was not found. Exiting.")
 	print("Device info:", device.serial_number)
-	if "PWND:[" not in device.serial_number:
-		sys.exit("[ERROR] Device's not in pwned DFU mode. Exiting.")
+	retassure("PWND:[" in device.serial_number, "Device's not in pwned DFU mode. Exiting.")
 def check_bb(identifier):
-	cellular_ipad = [
+	cellular_ipads = [
 			'iPad4,2',
 			'iPad4,3',
 			'iPad4,5',
@@ -51,4 +55,4 @@ def check_bb(identifier):
 			'iPad13,2']
 	if "iPhone" in identifier:
 		return True
-	return identifier in cellular_ipad
+	return identifier in cellular_ipads
