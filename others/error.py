42f9313db49227d27f0d7c16910cb42677409095
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-05 21:03:42 +0700

    update error handler

--- others/error.py
+++ others/error.py
@@ -1,8 +1,6 @@
-import sys
-
 def retassure(cond, err):
 	if not cond:
-		sys.exit(f"[ERROR] {err}")
+		raise Exception(err)
 
 def reterror(err):
-	sys.exit(f"[ERROR] {err}")
+	raise Exception(err)
