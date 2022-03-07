d517abe025db88b3f1a437900c3ea6dce2515da6
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-07 21:42:16 +0700

    Update error.py

--- others/error.py
+++ others/error.py
@@ -1,3 +1,9 @@
+import subprocess
+
+def detachret(cond, err, dir):
+	if not cond:
+		subprocess.run(('hdiutil','detach',dir), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		raise Exception(err)
 def retassure(cond, err):
 	if not cond:
 		raise Exception(err)
