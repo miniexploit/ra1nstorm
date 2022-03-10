1afcd47add928c6f07f1d2c66c12f61af8ee3ed2
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-10 09:18:07 +0700

    Update dependencies.py

--- others/dependencies.py
+++ others/dependencies.py
@@ -26,8 +26,8 @@ class check_requirements:
 				subprocess.check_call((binary, '-V'), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 			except subprocess.CalledProcessError:
 				reterror("This irecovery version is too old. Exiting.")
-			irecv_ver = subprocess.run((binary, '-V'), stdout=subprocess.PIPE, universal_newlines=True).stdout.replace("irecovery ","") # double check
-			retassure(int(irecv_ver[:-1].replace(".", "")) >= 101, f"Expected irecovery version to be greater than or equal to 1.0.1 but found version {irecv_ver}. Exiting.")
+			irecv_ver = subprocess.run((binary, '-V'), stdout=subprocess.PIPE, universal_newlines=True).stdout # double check
+			retassure("fork" in irecv_ver, "irecovery can't be used with ra1nstorm")
 		elif binary == "futurerestore":
 			fr_usage = subprocess.run((binary), stdout=subprocess.PIPE, universal_newlines=True).stdout
 			retassure(not any(("--ibss-img4" not in fr_usage, "--ibec-img4" not in fr_usage, "--rdsk" not in fr_usage, "--rkrn" not in fr_usage, "--skip-blob" not in fr_usage)), "This Futurerestore build does not allow specifying custom ramdisk and kernelcache. Exiting.")
