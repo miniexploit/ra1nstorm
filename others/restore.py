1c5f36f9a2200af9fe41314b8be910648d34b801
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-15 16:43:34 +0700

    remove apnonce blobs since not necessary

--- others/restore.py
+++ others/restore.py
@@ -9,11 +9,8 @@ class Restore:
 		self.device = device_struct
 		self.ipsw = ipsw
 
-	def save_blobs(self, save_path, apnonce):
-		if apnonce:
-			print("Saving temporary SHSH for signing bootchain...")
-		else:
-			print("Saving temporary SHSH for restoring...")
+	def save_blobs(self, save_path):
+		print("Saving temporary SHSH...")
 		args = [
 			'tsschecker',
 			'-d',
@@ -26,16 +23,10 @@ class Restore:
 			'-s',
 			'--save-path',
 			save_path,
-			'--nocache',
+			'--nocache'
 		]
-		if apnonce:
-			args.append('--apnonce')
-			args.append(self.device.apnonce) 
-		retassure(subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True).returncode == 0, "Failed to save SHSH blobs. Exiting.")
-		if '/apnonceblobs' in save_path:
-			self.apnonce_blob = glob.glob(f"{save_path}/*.shsh*")[0]
-		else:
-			self.blob = glob.glob(f"{save_path}/*.shsh*")[0]
+		retassure(subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True).returncode == 0, "Failed to save SHSH blobs")
+		self.blob = glob.glob(f"{save_path}/*.shsh*")[0]
 			
 	def sign_bootloader(self, path, output, type):
 		print(f"Signing {type}...")
@@ -51,7 +42,7 @@ class Restore:
 			'-T',
 			type.lower()
 		]
-		retassure(subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0, "Failed to sign bootloader. Exiting.")
+		retassure(subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0, "Failed to sign bootloader")
 		
 	def save_im4m(self, output, custom_blob=None):
 		print("Saving IM4M for signing bootchain...")
@@ -69,11 +60,11 @@ class Restore:
 				'img4tool',
 				'-e',
 				'-s',
-				self.apnonce_blob,
+				self.blob,
 				'-m',
 				output
 			]
-		retassure(subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0, "Failed to save IM4M. Exiting.")
+		retassure(subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode == 0, "Failed to save IM4M")
 		self.im4m = output
 
 	def restore(self, ibss, ibec, ramdisk, kernelcache, update, custom_blob=None, log_path=None):
@@ -122,10 +113,10 @@ class Restore:
 		if log_path:
 			if log_path.endswith('/'):
 				log_path = log_path[:-1]
-			froutput = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf8").stdout
+			result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf8")
 			with open(f'{log_path}/restore.log', 'w') as f:
-				f.write(froutput)
-			retassure('Done: restoring succeeded!' in froutput, f'Restore failed! Log saved to {log_path}/restore.log')
+				f.write(result.stdout)
+			retassure(result.returncode == 0, f'Restore failed ({result.returncode}). Log saved to {log_path}/restore.log')
 			print(f'Restore succeeded! Log saved to {log_path}/restore.log')
 		else:
-			subprocess.run(args, universal_newlines=True)
+			retasssure(subbprocess.run(args, universal_newlines=True).returncode == 0, f'Restore failed ({result.returncode})')
