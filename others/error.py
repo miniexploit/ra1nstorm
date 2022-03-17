127e2251982a6adef232e495aac3928989dcf672
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-17 13:47:41 +0700

    replace Exception with Ra1nstormException since python may catch an exception not raised by the program

--- others/error.py
+++ others/error.py
@@ -1,12 +1,16 @@
 import subprocess
 
+class Ra1nstormException(Exception):
+	pass
+
 def detachret(cond, err, dir):
 	if not cond:
 		subprocess.run(('hdiutil','detach',dir), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		raise Exception(err)
+		raise Ra1nstormException(err)
+
 def retassure(cond, err):
 	if not cond:
-		raise Exception(err)
+		raise Ra1nstormException(err)
 
 def reterror(err):
-	raise Exception(err)
+	raise Ra1nstormException(err)
