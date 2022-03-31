d8e765c35e9e8c2f85539831ca8ebb0a5d5c7562
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-31 21:50:17 +0700

    Update manifest.py

--- others/manifest.py
+++ others/manifest.py
@@ -6,10 +6,9 @@ class Manifest:
 		self.version = tuple(int(_) for _ in self.manifest['ProductVersion'].split('.'))
 		self.buildid = self.manifest['ProductBuildVersion']
 		self.supported_devices = self.manifest['SupportedProductTypes']
-	def get_buildid(self):
-		return self.manifest['ProductBuildVersion']
 		
 	def get_comp(self, board, comp):
 		for deviceclass in self.manifest['BuildIdentities']:
 			if deviceclass['Info']['DeviceClass'] == board:	
 				return deviceclass['Manifest'][comp]['Info']['Path']
+
