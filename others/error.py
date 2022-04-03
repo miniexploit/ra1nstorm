3ef7687800659343b8a5f3cdfe35b8473573d014
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-04-03 21:57:43 +0700

    Update error.py

--- others/error.py
+++ others/error.py
@@ -4,12 +4,12 @@ class Ra1nstormException(Exception):
 	pass
 
 def detachret(cond, err, dir):
-	if not cond:
+	if not (cond):
 		subprocess.run(('hdiutil','detach',dir), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 		raise Ra1nstormException(err)
 
 def retassure(cond, err):
-	if not cond:
+	if not (cond):
 		raise Ra1nstormException(err)
 
 def reterror(err):
